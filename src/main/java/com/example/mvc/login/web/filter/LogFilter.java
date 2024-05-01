327897c1ba60212896bb6e63af62f85ecb37948c
YuYoHan <zxzz8014@naver.com>
2024-05-01 09:28:45 +0900

    [Study] servlet filter

---
+++ src/main/java/com/example/mvc/login/web/filter/LogFilter.java
@@ -0,0 +1,42 @@
+package com.example.mvc.login.web.filter;
+
+import lombok.extern.log4j.Log4j2;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.UUID;
+
+@Log4j2
+public class LogFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        log.info("log filter init");
+    }
+
+    @Override
+    public void destroy() {
+        log.info("log filter destroy");
+    }
+
+    // HTTP 요청이 오면 이 메서드가 호출된다.
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        log.info("log filter doFilter");
+
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        String requestURI = httpRequest.getRequestURI();
+        // HTTP 요청을 구분하기 위해 요청당 임의의 uuid를 생성해준다.
+        String uuid = UUID.randomUUID().toString();
+
+        try {
+            log.info("REQUEST [{}][{}]", uuid, requestURI);
+            chain.doFilter(request, response);
+        } catch (Exception e) {
+            log.error("실팰했습니다. [{}]", e.getMessage());
+            throw e;
+        } finally {
+            log.info("RESPONSE [{}][{}]", uuid, requestURI);
+        }
+    }
+}
