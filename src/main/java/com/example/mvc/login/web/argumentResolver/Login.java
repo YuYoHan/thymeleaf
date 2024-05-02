53249c75ded23453c351413ea85faea57dfcd9b8
YuYoHan <zxzz8014@naver.com>
2024-05-02 21:14:26 +0900

    [Study] ArgumentResolver 사용

--- src/main/java/com/example/mvc/login/web/argumentResolver/Login.java
+++ src/main/java/com/example/mvc/login/web/argumentResolver/Login.java
@@ -6,6 +6,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Target(ElementType.PARAMETER)
+// 리플렉션 등을 활용할 수 있도록 런타임까지 애노테이션 정보가 남아 있음
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Login {
 }
