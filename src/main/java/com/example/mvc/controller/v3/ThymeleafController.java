52fb57e3f4184dea8dba2a9e5dff04dcffb1c3e5
YuYoHan <zxzz8014@naver.com>
2024-05-11 23:27:47 +0900

    [Study] 타임리프

--- src/main/java/com/example/mvc/controller/v3/ThymeleafController.java
+++ src/main/java/com/example/mvc/controller/v3/ThymeleafController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/thymeleaf")
@@ -17,7 +19,7 @@ public class ThymeleafController {
     @GetMapping("/ex01")
     public String ex01(Model model) {
         ItemDTOV3 item = ItemDTOV3.builder()
-                .itemName("상품1")
+                .itemName("상품")
                 .itemDetail("상품 설명")
                 .price(10000)
                 .stockNumber(10)
@@ -31,17 +33,21 @@ public class ThymeleafController {
 
     @GetMapping("/ex02")
     public String ex02(Model model) {
-        ItemDTOV3 item = ItemDTOV3.builder()
-                .itemName("상품1")
-                .itemDetail("상품 설명")
-                .price(10000)
-                .stockNumber(10)
-                .itemSellStatus(ItemSellStatus.SELL)
-                .regTime(LocalDateTime.now())
-                .build();
+        List<ItemDTOV3> items = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            ItemDTOV3 item = ItemDTOV3.builder()
+                    .itemName("상품" + i)
+                    .itemDetail("상품 설명" + i)
+                    .price(10000)
+                    .stockNumber(10)
+                    .itemSellStatus(ItemSellStatus.SELL)
+                    .regTime(LocalDateTime.now())
+                    .build();
+            items.add(item);
+        }
 
-        model.addAttribute("item", item);
-        return "thymeleaf/ex01";
+        model.addAttribute("items", items);
+        return "thymeleaf/ex02";
     }
 
 }
