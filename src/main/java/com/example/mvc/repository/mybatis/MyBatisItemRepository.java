b11f69ca18d2f77fdee53bd4b2b8902514d0b816
YuYoHan <zxzz8014@naver.com>
2024-05-26 23:42:34 +0900

    [Study] 리팩토링

--- src/main/java/com/example/mvc/repository/mybatis/MyBatisItemRepository.java
+++ src/main/java/com/example/mvc/repository/mybatis/MyBatisItemRepository.java
@@ -4,21 +4,22 @@ import com.example.mvc.domain.v1.ItemSearchCond;
 import com.example.mvc.domain.v1.ItemUpdateDTO;
 import com.example.mvc.domain.v1.ItemV1;
 import com.example.mvc.repository.item.v1.ItemRepositoryV1;
-import com.example.mvc.repository.item.v2.ItemRepositoryV2;
-import com.example.mvc.repository.v3.item.ItemRepositoryV3;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
+@Log4j2
 @RequiredArgsConstructor
 public class MyBatisItemRepository implements ItemRepositoryV1 {
     private final ItemMapper itemMapper;
 
     @Override
     public ItemV1 save(ItemV1 item) {
+        log.info("itemMapper class ={}", itemMapper.getClass() );
         itemMapper.save(item);
         return item;
     }
